a6aa87e97c0753ec0ae866f7dd9da9dbbdf48234
nero-noha <114581659+nero-noha@users.noreply.github.com>
2023-10-01 19:47:38 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,33 @@
+class ATM:
+    def __innit__(self, cardnumber, pin) :
+        self.cardnumber = cardnumber
+        self.pin = pin
+
+    def check_balance(self) :
+        print("Your balance is 50000")
+
+    def withdrawl(self, amount) :
+        new_amount = 50000 - amount
+        print("You have withdrawn amount"+str(amount) +". Your remaining balance is "+str(new_amount))
+
+def main() :
+    Card_number = input("Insert your card number :-")
+    pin_number = input("Insert your pin number :-")
+
+    new_user = ATM(Card_number, pin_number)
+
+    print("Choose your activity ")
+    print("1. Balance Enquiry   2. Withdrawl")
+    activity = int(input("Enter activity number here :-"))
+
+    if(activity == 1) :
+        new_user.check_balance()
+    elif(activity == 2) :
+        amount = int(input("Enter the amount :- "))
+        new_user.withdrawl(amount)
+    else :
+        print("Enter a valid  number")
+
+
+if __name__ == "__main__" :
+    main()
